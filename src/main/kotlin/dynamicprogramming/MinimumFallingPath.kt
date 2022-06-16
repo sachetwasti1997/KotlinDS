39cf54590ea7a1622e1d03d26da873bef2447cba
sachetwasti1997 <sachetwasti61@gmail.com>
2022-06-16 13:31:23 +0530

    Added Code

--- src/main/kotlin/dynamicprogramming/MinimumFallingPath.kt
+++ src/main/kotlin/dynamicprogramming/MinimumFallingPath.kt
@@ -1,8 +1,10 @@
 package dynamicprogramming
 
+
 fun minFallingPathSum(matrix: Array<IntArray>): Int {
     val m = matrix.size
     val n = matrix[0].size
+    val dp = Array(m){IntArray(n){-1}}
     fun dfs(i: Int, j: Int):Int{
         if(i == m || j == n || i == -1 || j == -1)return Int.MAX_VALUE
         if(i == 0){
@@ -14,8 +16,10 @@ fun minFallingPathSum(matrix: Array<IntArray>): Int {
             }
             return mx
         }
+        if(dp[i][j] != -1)return dp[i][j]
         val res = Math.min(dfs(i+1, j), Math.min(dfs(i+1, j+1), dfs(i+1, j-1)))
-        return matrix[i][j] + if(res == Int.MAX_VALUE)0 else res
+        dp[i][j] = matrix[i][j] + if(res == Int.MAX_VALUE)0 else res
+        return dp[i][j]
     }
 
     return dfs(0, 0)
